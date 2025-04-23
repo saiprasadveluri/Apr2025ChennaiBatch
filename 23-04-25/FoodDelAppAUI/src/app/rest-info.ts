2a30aae43d884a02d3ad6827db6b4364ac5163b9
HariRajaK <146092589+HariRajaK@users.noreply.github.com>
2025-04-23 17:57:01 +0530

    Add files via upload

---
+++ 23-04-25/FoodDelAppAUI/src/app/rest-info.ts
@@ -0,0 +1,12 @@
+export interface RestInfo {
+    restid?:string,
+    restName?:string,
+    restLoc?:string,
+    owner?:number
+}
+export interface RestInfoNew{
+    id?:string,
+    name?:string,
+    location?:string,
+    owner?:string
+}
