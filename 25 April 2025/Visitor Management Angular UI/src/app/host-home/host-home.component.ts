a30107a2c8482d26411811294a9c83b647540f95
NANDHAKUMAR S <92205301+nandhakumar-it20@users.noreply.github.com>
2025-04-26 09:59:46 +0530

    Add files via upload

---
+++ 25 April 2025/Visitor Management Angular UI/src/app/host-home/host-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-host-home',
+  templateUrl: './host-home.component.html',
+  styleUrls: ['./host-home.component.css']
+})
+
+export class HostHomeComponent {
+
+}
---
+++ 25 April 2025/Visitor Management Angular UI/src/app/manager-dashboard/manager-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-dashboard',
+  templateUrl: './manager-dashboard.component.html',
+  styleUrls: ['./manager-dashboard.component.css']
+})
+
+export class ManagerDashboardComponent {
+
+}
