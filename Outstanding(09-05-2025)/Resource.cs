01b79c9abb9504eea2492debfa01e8ba5b6093f0
soumya1716 <chunchusoumya2000@gmail.com>
2025-05-09 17:48:23 +0530

    Add files via upload

---
+++ Outstanding(09-05-2025)/Resource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outstanding
+{
+    internal abstract class Resource
+    {
+            public string Title { get; }
+            public string Author { get; }
+            public int ChapterCount { get; }
+
+            //Constructor
+            public Resource(string title, string author, int chapterCount)
+            {
+                Title = title;
+                Author = author;
+                ChapterCount = chapterCount;
+            }
+            public abstract bool IsOutstanding();
+        
+    }
+}
