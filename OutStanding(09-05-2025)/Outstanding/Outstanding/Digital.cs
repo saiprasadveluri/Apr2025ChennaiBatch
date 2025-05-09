9f7e0ce7c407000534128d2909f5ce90876ca570
Sowmikanallavolu2532 <sowmika2532@gmail.com>
2025-05-09 17:49:53 +0530

    Add files via upload

---
+++ OutStanding(09-05-2025)/Outstanding/Outstanding/Digital.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outstanding
+{
+    internal class Digital:Resource
+    {
+
+        public double PlayLength { get; }
+        public string FileFormat { get; }
+        public double AverageReviewPoints { get; }
+        private const double REVIEWPOINTS_THRESHOLD = 4.5;
+        public Digital(double playLength, string fileFormat, double reviewAverage,
+            string title, string author, int chapterCount) : base(title, author, chapterCount)
+        {
+            PlayLength = playLength;
+            FileFormat = fileFormat;
+            AverageReviewPoints = reviewAverage;
+        }
+
+        public override bool IsOutstanding()
+        {
+            return AverageReviewPoints >= REVIEWPOINTS_THRESHOLD;
+        }
+    }
+}
---
+++ OutStanding(09-05-2025)/Outstanding/Outstanding/Resource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outstanding
+{
+    internal abstract class Resource
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int chapterCount { get; set; }
+        public Resource(string title,string author,int chaptercount) 
+        { 
+            Title = title;
+            Author = author;
+            chapterCount= chaptercount;
+
+        }
+           public abstract bool IsOutstanding();
+    }
+}
