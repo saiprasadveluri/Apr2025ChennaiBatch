b2e8abb7176fdd58ec525b50b37a82c0920e5e30
saiprasadveluri <59499218+saiprasadveluri@users.noreply.github.com>
2025-05-09 10:17:52 +0530

    RideAggregatorOOPS Solution
    RideAggregatorOOPS Solution

---
+++ Class Assignment 09_05_2025/RideAggregatorOOPS/RideAggregatorOOPS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideAggregatorOOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a driver & Customer
+            Driver driver = new Driver("Sai", "99887766", 23, 4.7);
+            Customer customer = new Customer("Durga", "76545", "Hyderabad");
+            //Create Ride
+            string pickup = "Central";
+            string drop = "KPHB";
+            Ride ride= new Ride(driver, customer, pickup, drop);
+            //Perform actions
+            string startStatus= ride.StartRide();
+            Console.WriteLine(startStatus);
+            string completionStatus=ride.CompleteRide();
+            Console.WriteLine(completionStatus);
+            string billDetails=ride.GenerateBill();
+            Console.WriteLine(billDetails);
+        }
+    }
+}
---
+++ Class Assignment 09_05_2025/RideAggregatorOOPS/RideAggregatorOOPS/Ride.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideAggregatorOOPS
+{
+    internal class Ride
+    {
+        private Customer RideCustomer {  get; set; }//'Has a' Relation
+        private Driver RideDriver { get; set; }//'Has a' relations
+        //Other Props
+        public string PickupLocation {  get; set; }
+        public string DropLocation {  get; set; }
+        public string StartRide()
+        {
+            return string.Format($"Ride Started from {PickupLocation}");
+        }
+        public string CompleteRide()
+        {
+            return string.Format($"Ride Ended at {DropLocation}");
+        }
+        public string GenerateBill()
+        {
+            String strOut = String.Empty;
+            string driverDetails = GetDriverDetails();//Delegation Model
+            string customerDetails = GetCusomerDetails();  //Delegation Model         
+            return String.Format($"Bill Genrated...{driverDetails}....{customerDetails}");
+        }
+        //Private Functions
+        private string GetDriverDetails()
+        {
+            return RideDriver.GetDriverInfo();//Delegation Model
+        }
+
+        private string GetCusomerDetails()
+        {
+            return RideCustomer.GetCustomerInfo();//Delegation Model
+        }
+        //Constructor
+        public Ride(Driver driver,Customer customer,string pickup,string drop)
+        {
+            RideDriver = driver;
+            RideCustomer = customer;
+            PickupLocation = pickup;
+            DropLocation = drop;
+        }
+    }
+}
