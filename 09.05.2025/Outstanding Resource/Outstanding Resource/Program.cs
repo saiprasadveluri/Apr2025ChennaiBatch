b6da0475100958511a4b4d90dbbdaff1cf29bca0
Harivar <109642763+Harivar@users.noreply.github.com>
2025-05-09 16:30:45 +0530

    Add files via upload

---
+++ 09.05.2025/Outstanding Resource/Outstanding Resource/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outstanding_Resource
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Resource resource1 = new Paperback("Programming","Sphere","20","1234567890",300,12);
+            Resource resource2 = new Paperback("Coding", "Shakes", "15", "112233445566", 250, 9);
+            Resource resource3 = new Digital("Design patterns","George","15","8h 30 min","PDF",3.5);
+            Resource resource4 = new Digital("Data Analysis", "Albert", "25", "10h 30 min", "PDF", 5);
+            
+            Console.WriteLine("Outstanding Resources");
+
+            if(resource1.IsOutStanding())
+            {
+                Console.WriteLine($"{resource1.Title} by {resource1.Author}");
+            }
+            if (resource2.IsOutStanding())
+            {
+                Console.WriteLine($"{resource2.Title} by {resource2.Author}");
+            }
+            if (resource3.IsOutStanding())
+            {
+                Console.WriteLine($"{resource3.Title} by {resource3.Author}");
+            }
+            if (resource4.IsOutStanding())
+            {
+                Console.WriteLine($"{resource4.Title} by {resource4.Author}");
+            }
+
+
+            
+        }
+    }
+}
---
+++ 09.05.2025/Outstanding Resource/Outstanding Resource/Resource.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outstanding_Resource
+{
+    abstract class Resource
+    {
+        public string Title {  get; set; }
+        public string Author { get; set; }
+        public string NumberOfChapters { get; set; }
+
+        public Resource(string title,string author,string Num_Of_Chapters) 
+        { 
+            Title = title;
+            Author = author;
+            NumberOfChapters= Num_Of_Chapters;
+        }
+
+        public abstract bool IsOutStanding();
+    }
+
+    class Paperback : Resource
+    {
+        public string ISBNcode { get; set; }
+        public int NumberOfPages { get; set; }
+        public int NumberOfBorrowers { get; set; }
+
+        public Paperback(string title, string author, string Num_Of_Chaptersstring, 
+            string ISBNCode, int numberOfPages, int numberOfBorrowers): base(title, author, Num_Of_Chaptersstring)
+        {
+            ISBNcode = ISBNCode;
+            NumberOfPages = numberOfPages;
+            NumberOfBorrowers = numberOfBorrowers;
+        }
+
+        public override bool IsOutStanding()
+        {
+            return NumberOfBorrowers > 10;
+        }
+
+
+    }
+
+    class Digital : Resource
+    {
+        public string Playlength { get; set; }
+        public string FileFormat { get; set; }
+        public double AverageReviewPoints {  get; set; }
+
+        public Digital(string title, string author, string Num_Of_Chaptersstring,
+            string playlength, string fileFormat, double averageReviewPoints) :
+            base(title, author, Num_Of_Chaptersstring)
+        {
+            Playlength = playlength;
+            FileFormat = fileFormat;
+            AverageReviewPoints = averageReviewPoints;
+        }
+
+        public override bool IsOutStanding()
+        {
+            return AverageReviewPoints > 4.5;
+        }
+    }
+
+
+
+}
