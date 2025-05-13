373c9c002cec451375ffc17a188b93bd8d7d98f9
PremKumarRevalla <111066517+PremKumarRevalla@users.noreply.github.com>
2025-05-13 13:54:30 +0530

    Add files via upload

---
+++ 9th May 2025/TaskManagerPMTool/TaskManagerPMTool/Bug.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerPMTool
+{
+    internal class Bug : Tasks
+    {
+        public string TaskType { get; set; }
+        public string SourceFileName { get; set; }
+        public int LineNumber { get; set; }
+        public string Severity { get; set; }
+
+        public Bug(string taskType, string sourceFileName, int lineNumber, string severity, string title, string description, DateTime startDate, DateTime endDate, double estmTime, string priority, string status, string projectTitle, Users createdBy):base(title, description,startDate,endDate,estmTime, priority, status, projectTitle, createdBy) 
+        { 
+            TaskType = taskType;
+            SourceFileName = sourceFileName;
+            LineNumber = lineNumber;
+            Severity = severity;
+        }
+
+        public override bool Task_Type()
+        {
+            return TaskType == "BUG";
+        }
+
+        public override string GetTaskInfo()
+        {
+            string baseDetails = base.GetTaskInfo();
+            return string.Format($"{baseDetails} \n Bug Details Source File Name - {SourceFileName} \n Line Number - {LineNumber} \n Severity - {Severity}");
+        }
+    }
+}
---
+++ 9th May 2025/TaskManagerPMTool/TaskManagerPMTool/Users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerPMTool
+{
+    internal class Users
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        public Users(string userName, string email, string password, string role)
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+            Role = role;
+        }
+
+        public string GetUserInfo()
+        {
+            return string.Format($"User Name-{UserName} \n Email-{Email} \n Password-{Password} \n Role-{Role}");
+        }
+    }
+}
