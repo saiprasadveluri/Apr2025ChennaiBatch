6c554874bac7c6f52cedd59187bd7c3bef55d3ef
Sowmikanallavolu2532 <sowmika2532@gmail.com>
2025-05-13 10:33:51 +0530

    Add files via upload

---
+++ TaskManager/TaskManager/TaskManager/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    internal class User
+    {
+        string userName { get; set; }
+        string email { get; set; }
+        string password { get; set; }
+        string role { get; set; }
+
+        public User(string uname, string Email, string pswd, string Role)
+        {
+            userName = uname;
+            email = Email;
+            password = pswd;
+            role = Role;
+        }
+        public void showDetails()
+        {
+            Console.WriteLine($
+        }
+
+    }
+}
