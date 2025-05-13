accd6ba5a4db16903a19575fffa3b6bbaa43c6b3
shrinidhisaravanan1011 <shrinidhisaravanan1011@gmail.com>
2025-05-13 16:37:55 +0530

    Add files via upload

---
+++ comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projectmanager
+{
+    public class comment
+    {
+        public string Text { get; set; }
+        public User Author { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
---
+++ project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projectmanager
+{
+   public class project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public User ProjectManager { get; set; }
+        public List<User> TeamMembers { get; set; } = new();
+        public List<TaskBase> Tasks { get; set; } = new();
+    }
+}
