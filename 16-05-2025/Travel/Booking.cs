1551c5222c68ef2b32c1f9c029285b626d39c18f
charpitha-1 <thavishala279@gmail.com>
2025-05-16 20:08:42 +0530

    Add files via upload

---
+++ 16-05-2025/Travel/Booking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelEzeeConsole
+{
+    internal class Booking
+    {
+        public int Source { get; set; }
+        public int Destination { get; set; }
+        public int Seatno{ get; set; }
+        public override string ToString()
+        {
+            return $"Source: {Source} - Destination: {Destination} - SeatNo: {Seatno}";
+        }
+
+        public string GetBookingdetails()
+        {
+            return $"{Source},{Destination},{Seatno}";
+        }
+
+    }
+}
