524fc138e08742566c95c66547b9b36c43f69376
vamsidamerla <vamsivikky7@gmail.com>
2025-05-17 10:32:29 +0530

    Add files via upload

---
+++ TravelEzeeConsole/TravelEzeeConsole/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelEzeeConsole
+{
+    internal class Booking
+    {
+        public int BookingId { get; set; }
+        public string ServNo {  get; set; }
+        public DateTime TravelDate { get; set; }
+        public List<PassengerEntry> PassengerList { get; set; }
+
+    }
+}
