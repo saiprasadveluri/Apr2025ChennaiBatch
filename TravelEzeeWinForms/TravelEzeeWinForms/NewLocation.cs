4bb15c1716b6b722bf0a8cfd8d959f1a7c372ab6
saiprasadveluri <59499218+saiprasadveluri@users.noreply.github.com>
2025-05-17 15:27:03 +0530

    TravelEzee with Win Form Frontend
    TravelEzee with Win Form Frontend

---
+++ TravelEzeeWinForms/TravelEzeeWinForms/NewLocation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TravelEzeeConsole;
+
+namespace TravelEzeeWinForms
+{
+    public partial class NewLocation : Form
+    {
+        AdminModule _adminModule;
+        public NewLocation(AdminModule adminModule)
+        {
+            _adminModule= adminModule;
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtLocId.Text) || string.IsNullOrEmpty(txtLocName.Text))
+            {
+                MessageBox.Show("Enter Details");
+            }
+            else
+            {
+                TravelEzeeConsole.Location loc=new TravelEzeeConsole.Location();
+                loc.LocationId=int.Parse(txtLocId.Text);
+                loc.Name=txtLocName.Text;
+                _adminModule.AddLocation(loc);
+                MessageBox.Show("Location Added Successfully");
+                DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
