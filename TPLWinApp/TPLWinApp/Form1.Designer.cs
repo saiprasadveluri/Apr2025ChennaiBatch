2569e6e72564b9796123e21d6b37441f16c1b7dd
saiprasadveluri <59499218+saiprasadveluri@users.noreply.github.com>
2025-05-22 12:52:37 +0530

    Task Async Programming-1

---
+++ TPLWinApp/TPLWinApp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TPLWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstActions = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(272, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstActions
+            // 
+            this.lstActions.FormattingEnabled = true;
+            this.lstActions.Location = new System.Drawing.Point(26, 12);
+            this.lstActions.Name = "lstActions";
+            this.lstActions.Size = new System.Drawing.Size(338, 186);
+            this.lstActions.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(272, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lstActions);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstActions;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TPLWinApp/TPLWinApp/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPLWinApp
+{
+    public partial class Form1 : Form
+    {
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            /*Task<string> downloadTask = new Task<string>(() =>
+            {
+                lstActions.Items.Add("From downlaod Task,,, downloading");
+                Task.Delay(5000).Wait();
+                lstActions.Items.Add("From downlaod Task,,, complete");
+                return "FileName.txt";
+            });            
+            var ContTask=downloadTask.ContinueWith<string>(t =>
+            {
+                string Downloadfile = t.Result;
+                lstActions.Items.Add("File Save started..." + Downloadfile);
+                Task.Delay(5000).Wait();
+                return "Success";
+            });
+            downloadTask.Start();
+            string FinalResult= ContTask.Result;
+            lstActions.Items.Add("Final Process Result: "+FinalResult);*/
+            /*Task<int> t = new Task<int>(() =>
+            {    
+                for(int n=1;n<5;++n)
+                {
+                    Task childTask = new Task(() =>
+                    {
+                        lstActions.Items.Add("From Child Task");
+                    },TaskCreationOptions.AttachedToParent);
+                    childTask.Start();
+                }
+                lstActions.Items.Add("Parent Completed");
+                return 10;
+            });
+            t.ContinueWith(ptask => {
+                lstActions.Items.Add("From Task COntinuation");
+            },TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            try
+            {
+                t.Start();
+                while(t.Status!= TaskStatus.RanToCompletion)
+                {
+                    lstActions.Items.Add("Parent is running...");
+                    Task.Delay(200).Wait();
+                }
+                var val=t.Result;
+            }
+            catch (Exception ex)
+            {
+                lstActions.Items.Add(ex.GetType()+ " : "+ ex.Message);
+                if(ex.InnerException!=null)
+                {
+                    lstActions.Items.Add(ex.InnerException.Message);
+                }
+            }*/
+
+            /*Task longRunning = new Task(() =>
+            {
+                for (int i = 0; i < 100000; ++i)
+                {
+                    Task.Delay(100).Wait();
+                    if(cancellationTokenSource.IsCancellationRequested)
+                    {
+                        cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                        break;
+                    }
+                    lstActions.Items.Add("Counting: "+i);
+                }
+            }, cancellationTokenSource.Token);
+            Task onCacelAction = longRunning.ContinueWith(t =>
+            {
+                lstActions.Items.Add("Cancellation requested");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+            longRunning.Start();*/
+            /*Task task = new Task(MyTaskFunc);
+            task.Start();
+            task.Wait();
+            lstActions.Items.Add("Action comleted");*/
+            await MyTaskFunc();
+            lstActions.Items.Add("Action comleted");
+        }
+
+        private async Task MyTaskFunc()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                lstActions.Items.Add("Counting: " + i);                
+                await MyTaskFunc_2();
+            }
+        }
+        private async Task MyTaskFunc_2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                lstActions.Items.Add("Counting_2: " + i);
+                await Task.Delay(100);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource.Cancel();
+        }
+    }
+}
