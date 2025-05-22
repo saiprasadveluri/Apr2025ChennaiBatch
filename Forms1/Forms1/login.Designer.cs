de3e4963d2f978b87510e17e0d5416c1ae22fb16
Trinay5 <113661792+Trinay5@users.noreply.github.com>
2025-05-22 18:02:21 +0530

    Add files via upload

---
+++ Forms1/Forms1/login.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Forms1
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Emailid = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Roles = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email Id";
+            // 
+            // Emailid
+            // 
+            this.Emailid.Location = new System.Drawing.Point(325, 46);
+            this.Emailid.Name = "Emailid";
+            this.Emailid.Size = new System.Drawing.Size(155, 20);
+            this.Emailid.TabIndex = 1;
+            this.Emailid.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(325, 82);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(155, 20);
+            this.password.TabIndex = 1;
+            this.password.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Role";
+            // 
+            // Roles
+            // 
+            this.Roles.FormattingEnabled = true;
+            this.Roles.Items.AddRange(new object[] {
+            "ADMIN",
+            "OWNER",
+            "USER"});
+            this.Roles.Location = new System.Drawing.Point(325, 123);
+            this.Roles.Name = "Roles";
+            this.Roles.Size = new System.Drawing.Size(155, 21);
+            this.Roles.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Roles);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Emailid);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login Page";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.Click += new System.EventHandler(this.Login_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Emailid;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox Roles;
+        private System.Windows.Forms.Button button1;
+    }
+}
